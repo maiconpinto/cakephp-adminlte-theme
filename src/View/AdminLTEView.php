a5039a95aa42119a8bc357c3eca9f46f4cea9834
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-06-28 17:40:19 -0300

    WIP #38 - extended AppView

---
+++ src/View/AdminLTEView.php
@@ -0,0 +1,45 @@
+<?php
+namespace AdminLTE\View;
+
+use \Cake\View\View;
+use \Cake\Core\App;
+use \Cake\Utility\Inflector;
+
+class AdminLTEView extends View
+{
+    protected function _paths($plugin = null, $cached = true)
+    {
+        $prefix = $this->request->getParam('prefix') ? Inflector::camelize($this->request->getParam('prefix')) : false;
+        $theme = $this->theme;
+
+        $templatePaths = App::path(static::NAME_TEMPLATE);
+
+        foreach ($templatePaths as $templateCurrent) {
+            if (!empty($theme)) {
+                if (!empty($plugin)) {
+                    for ($i = 0, $count = count($templatePaths); $i < $count; $i++) {
+                        if ($prefix) {
+                            $pluginPaths[] = $templatePaths[$i] . 'Plugin'.  DIRECTORY_SEPARATOR . $theme . DIRECTORY_SEPARATOR . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR . $prefix . DIRECTORY_SEPARATOR;
+                        }
+
+                        $pluginPaths[] = $templatePaths[$i] . 'Plugin'.  DIRECTORY_SEPARATOR . $theme . DIRECTORY_SEPARATOR . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR;
+                    }
+                }
+
+                if ($prefix) {
+                    $themePaths[] = $templateCurrent . 'Plugin'.  DIRECTORY_SEPARATOR . $theme . DIRECTORY_SEPARATOR . $prefix . DIRECTORY_SEPARATOR;
+                }
+
+                $themePaths[] = $templateCurrent . 'Plugin'.  DIRECTORY_SEPARATOR . $theme . DIRECTORY_SEPARATOR;
+            }
+        }
+
+        $paths = array_merge(
+            $pluginPaths,
+            $themePaths,
+            parent::_paths($plugin, $cached)
+        );
+
+        return $this->_paths = $paths;
+    }
+}
