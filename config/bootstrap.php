8bc4d78bbe67c2a70028ec6225a9bcc4dbe1a426
tsmsogn <tsmsogn@gmail.com>
2018-04-19 12:23:40 +0900

    Split configs

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,17 +3,12 @@ use Cake\Core\Configure;
 use Cake\Core\Exception\MissingPluginException;
 use Cake\Core\Plugin;
 
-Configure::write('Theme', [
-    'title' => 'AdminLTE',
-    'logo' => [
-        'mini' => '<b>A</b>LT',
-        'large' => '<b>Admin</b>LTE'
-    ],
-    'login' => [
-        'show_remember' => true,
-        'show_register' => true,
-        'show_social' => true
-    ],
-    'folder' => ROOT,
-    'skin' => 'blue'
-]);
+Configure::write('Theme.title', 'AdminLTE');
+Configure::write('Theme.logo.mini', '<b>A</b>LT');
+Configure::write('Theme.logo.large', '<b>Admin</b>LTE');
+Configure::write('Theme.login.show_remember', true);
+Configure::write('Theme.login.show_register', true);
+Configure::write('Theme.login.show_social', true);
+Configure::write('Theme.login.show_social', true);
+Configure::write('Theme.folder', ROOT);
+Configure::write('Theme.skin', 'blue');
